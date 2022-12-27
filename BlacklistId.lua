f76fa73b7bb5770621c060a41ba6e67512dea5f0
grekkk <110134687+grekkk@users.noreply.github.com>
2022-12-27 04:23:14 +0100

    Delete BlacklistId.lua

--- BlacklistId.lua
+++
@@ -1,7 +0,0 @@
-local function Blacklist(UserID, Reason)
-	if game:GetService('Players').LocalPlayer.UserId == UserID then
-		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BANNED: \n '..Reason)
-		while true do end
-	end
-end
-Blacklist(3655599610, "No reason provided.")
